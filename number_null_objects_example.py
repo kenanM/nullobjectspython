22ca0be2615a5590dfc51e081041f619142c616d
Kenan McGrath <kenan.mcgrath@genomicsengland.co.uk>
2018-09-19 12:44:54 +0100

    Kenans answer

--- number_null_objects_example.py
+++ number_null_objects_example.py
@@ -2,17 +2,8 @@ from optional import optional_of_nullable, optional_of, empty
 
 
 def product(x, y):
-    if x is None or y is None:
-        return 0
-    else:
-        return x * y
+    return x.get(default=0) * y.get(default=0)
 
 
 def sum(x, y):
-    if x is None and y is None:
-        return 0
-    elif x is None:
-        return y
-    elif y is None:
-        return x
-    return x + y
+    return x.get(default=0) + y.get(default=0)
--- test_numbers_null_objects.py
+++ test_numbers_null_objects.py
@@ -6,13 +6,13 @@ from optional import optional_of_nullable, optional_of, empty
 
 class NumbersNullObjectsTest(TestCase):
     def test_null_product_example(self):
-        assert product(5, None) == 0
+        assert product(optional_of(5), empty()) == 0
 
     def test_product_actually_works_with_some_numbers(self):
-        assert product(5, 5) == 25
+        assert product(optional_of(5), optional_of(5)) == 25
 
     def test_null_sum_example(self):
-        assert sum(None, 5) == 5
+        assert sum(empty(), optional_of(5)) == 5
 
     def test_sum_actually_works_with_some_numbers(self):
-        assert sum(5, 5) == 10
+        assert sum(optional_of(5), optional_of(5)) == 10
